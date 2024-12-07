d2b889edefce366eb5a5f8a7c53e9c0a1090cc9d
zaeem2396 <zaeemansari87@gmail.com>
2024-12-07 19:57:42 +0530

    chore: modified migration file

---
+++ migrations/Version20241207142341.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20241207142341 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'create article table';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE article (id INT AUTO_INCREMENT NOT NULL, author_name VARCHAR(255) NOT NULL, title VARCHAR(255) NOT NULL, summary LONGTEXT NOT NULL, image VARCHAR(255) NOT NULL, created_at DATETIME NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('DROP TABLE blogCategory');
+        $this->addSql('DROP TABLE blogs');
+        $this->addSql('DROP TABLE users');
+        $this->addSql('DROP TABLE migrations');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE article');
+    }
+}
